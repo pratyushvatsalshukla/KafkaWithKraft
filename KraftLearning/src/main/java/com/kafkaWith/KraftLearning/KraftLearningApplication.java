9e73ee536fd2938779e75303f261771396864b18
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-01-31 23:19:10 +0530

    Initialized Spring Boot Project

---
+++ KraftLearning/src/main/java/com/kafkaWith/KraftLearning/KraftLearningApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaWith.KraftLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KraftLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KraftLearningApplication.class, args);
+	}
+
+}
---
+++ KraftLearning/src/test/java/com/kafkaWith/KraftLearning/KraftLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaWith.KraftLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KraftLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
