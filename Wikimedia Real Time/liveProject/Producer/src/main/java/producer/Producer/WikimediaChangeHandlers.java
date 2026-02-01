404867d149293f546c63296b62eeb88e6f95284e
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-02-01 16:46:00 +0530

    Added Wikimedia Change Handler, this fetches change from wikimedia stream and logs / sends to consumer

---
+++ Wikimedia Real Time/liveProject/Producer/src/main/java/producer/Producer/WikimediaChangeHandlers.java
@@ -0,0 +1,34 @@
+package producer.Producer;
+
+import com.launchdarkly.eventsource.MessageEvent;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import tools.jackson.databind.ObjectMapper;
+
+//import org.apache.kafka.clients.consumer.internals.events.BackgroundEventHandler;
+@Slf4j
+@RequiredArgsConstructor
+public class WikimediaChangeHandlers implements BackgroundEventHandler {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onOpen() {
+        log.info("Stream Opened...");
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+        String data = messageEvent.getData();
+        log.info("Event Data -> {}", data);
+        kafkaTemplate.send("wikimedia-topic", data);
+    }
+
+    @Override public void onClosed() {}
+    @Override public void onComment(String comment) {}
+    @Override public void onError(Throwable t) { log.error("Error", t); }
+}
