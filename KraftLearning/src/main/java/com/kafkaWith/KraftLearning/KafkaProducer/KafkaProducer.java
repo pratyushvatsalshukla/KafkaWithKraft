6ba57288f2820348912666e069ae5b7c976e23b0
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-02-01 12:57:01 +0530

    Defined Kafka Producer

---
+++ KraftLearning/src/main/java/com/kafkaWith/KraftLearning/KafkaProducer/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.kafkaWith.KraftLearning.KafkaProducer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class KafkaProducer {
+
+    private KafkaTemplate<String,String> kafkaTemplate ;
+
+    public KafkaProducer(KafkaTemplate<String,String> kafkaTemplate){
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message){
+        log.info("Sending .... {}", message);
+        kafkaTemplate.send("kafkaLearningTopic",message) ;
+    }
+}
