a699cb25441b25f26f1ea5191dda5dc5adcc39d6
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-02-01 14:31:54 +0530

    Added API for Publishing Json Messages

--- KraftLearning/src/main/java/com/kafkaWith/KraftLearning/Controller/MessageController.java
+++ KraftLearning/src/main/java/com/kafkaWith/KraftLearning/Controller/MessageController.java
@@ -1,24 +1,34 @@
 package com.kafkaWith.KraftLearning.Controller;
 
+import com.kafkaWith.KraftLearning.Entity.Student;
+import com.kafkaWith.KraftLearning.KafkaProducer.JsonKafkaProducer;
 import com.kafkaWith.KraftLearning.KafkaProducer.KafkaProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/kafka")
 @RequiredArgsConstructor
+@Slf4j
 public class MessageController {
 
     private final KafkaProducer kafkaProducer ;
+    private final JsonKafkaProducer jsonKafkaProducer ;
     //http://localhost:8080/api/1/kafka/publish?message=hello kafka
     @GetMapping("/publish")
     public ResponseEntity<?> publishMessage(@RequestParam("message") String message){
         kafkaProducer.sendMessage(message);
+        return new ResponseEntity<>("Message Sent....", HttpStatus.OK) ;
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<?> publishJsonMessage(@RequestBody Student student){
+        log.info("Sending Message .....");
+        jsonKafkaProducer.sendMessage(student);
+        log.info("Message Sent....");
         return new ResponseEntity<>("Message Sent....", HttpStatus.OK) ;
     }
 }
