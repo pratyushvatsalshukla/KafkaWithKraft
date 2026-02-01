4b9f05c1783b3e2de8b97d360edc6e8930c7c1ce
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-02-01 14:31:25 +0530

    Created Simple Entity to deal with RequestBody

---
+++ KraftLearning/src/main/java/com/kafkaWith/KraftLearning/Entity/Student.java
@@ -0,0 +1,15 @@
+package com.kafkaWith.KraftLearning.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+    private String studentName ;
+    private String studentRollNumber ;
+}
