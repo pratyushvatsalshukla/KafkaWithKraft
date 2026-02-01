32f8d1171a3230a9cba05109f3ff73a7184c87e7
pratyushvatsalshukla <pratyushpro9333@gmail.com>
2026-02-01 15:10:49 +0530

    Initialized The Parent For Live Project

---
+++ Wikimedia Real Time/liveProject/src/main/java/com/wikimedia/liveProject/LiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wikimedia.liveProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveProjectApplication.class, args);
+	}
+
+}
